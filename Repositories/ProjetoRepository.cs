a678fe15c620584bb1036c09c2936d596bd6ac8e
JHbarreto <jose.henrique.paes.barreto@gmail.com>
2023-04-26 17:18:10 -0300

    adicionando metodos http

--- Repositories/ProjetoRepository.cs
+++ Repositories/ProjetoRepository.cs
@@ -18,5 +18,34 @@ namespace Exo.WebApi.Repositories
         {
             return _context.Projetos.ToList();
         }
+
+        //Complemento do crud feito na atividade anterior
+        public void Cadastrar(Projeto projeto)
+        {
+            _context.Projetos.Add(projeto);
+            _context.SaveChanges();
+        }
+        public Projeto BuscarporId(int Id)
+        {
+            return _context.Projetos.Find(Id);
+        }
+        public void Atualizar(int id, Projeto projeto)
+        {
+            Projeto projetoBuscado = _context.Projetos.Find(id);
+            if (projetoBuscado != null)
+            {
+                projetoBuscado.NomeDoProjeto = projeto.NomeDoProjeto;
+                projetoBuscado.Area = projeto.Area;
+                projetoBuscado.Status = projeto.Status;
+            }
+            _context.Projetos.Update(projetoBuscado);
+            _context.SaveChanges();
+        }
+        public void Deletar(int id)
+        {
+            Projeto projetoBuscado = _context.Projetos.Find(id);
+            _context.Projetos.Remove(projetoBuscado);
+            _context.SaveChanges();
+        }
     }
 }
